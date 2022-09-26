7c61cea4daa9961294cb453341b5e55c1d4b18b7
muhamadfad <98180195+muhamadfad@users.noreply.github.com>
2022-09-26 21:24:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,74 +11,32 @@
  * number of geometry stacks and slices can be adjusted
  * using the + and - keys.
  */
-#include <windows.h>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+
+#include <windows.h>;
 #include <GL/glut.h>
-#endif
 
-#include <stdlib.h>
-
-#include <windows.h>
-#ifdef _APPLE_
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
-#include <stdlib.h>
-
-
-void Draw() {
-glClear(GL_COLOR_BUFFER_BIT);
-
-glPointSize(9.0);                   // ini ukuran titiknya
-
-glColor3f(1.1, 1.0, 1.0);           // digunakan untuk merubah warna yaitu warna putih
-glBegin(GL_POINTS);                 // awal kode untuk menggambar
-
-glColor3f(1.0, 0.0, 0.0);           // Ini warna merah guys
-glVertex3f(0.9, 0.9, 0.1);          // posisi titik yang akan digambar
-
-glColor3f(0.0, 0.0, 1.0);           // Ini warna Biru guys
-glVertex3f(0.8, 0.8, 0.1);
-
-glColor3f(1.0, 1.0, 0.0);// Ini warna Hijau
-glVertex3f(0.7, 0.7, 0.1);
-
-glColor3f(1.0, 0.5, 0.0);           // Ini Orange Guys
-glVertex3f(0.6, 0.6, 0.1);
-
-glColor3f(0.0, 1.0, 0.0);
-glVertex3f(0.5, 0.5, 0.1);
-
-glColor3f(1.0, 0.6, 0.7);           // Ini warna apa ini
-glVertex3f(0.4, 0.4, 0.1);
-
-glEnd();                            // akhir kode untuk menggambar
-glFlush();                          // memastikan bahwa kode dieksekusi
+void garisBiru() {
+    glBegin(GL_LINES); //sintaks awal menggambarkan garis
+    glColor3f(0.0,0.0,1.0); //brush pen
+    glVertex2i(100,200); // koordinat titim awal (x,y)
+    glVertex2i(250,200);
+    glEnd(); //mengakhiri gambaran
+    glFlush(); //memulai proses penggambaran
 }
 
-
-
-void Initialize() {
-glClearColor(0.0, 0.0, 0.0, 0.0);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT); //membersihkan buffer
+    garisBiru();
 }
 
-
-
-int main(int iArgc, char** cppArgv) {
-glutInit(&iArgc, cppArgv);
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize(250, 250);
-glutInitWindowPosition(200, 200);
-glutCreateWindow("Latihan ");
-Initialize();
-glutDisplayFunc(Draw);
-glutMainLoop();
-return 0;
+int main (int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitWindowPosition(250,250); //posisi window/jendelanya
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Menggambar garis biru"); //title bar pd window
+    glClearColor(1.0,0.0,0.0,0.0); //warna baground merah
+    gluOrtho2D(0.0,500.0,-250,250); //besar sistem koordinat dengan range (x,x,y,y)
+    glutDisplayFunc(display);
+    glutMainLoop(); //
+    return 0;
 }
